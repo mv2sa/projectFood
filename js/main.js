f04aef2db0957b6a8c0a567bb23044f440b062a1
Marcos Santanna <marcosv.santanna@hotmail.com>
2014-08-25 17:30:44 -0400

    angular hook-ups, removal of font awesome css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+var app = angular.module('findMyFood', ['ui.router']);
 
+app.config(function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/dashboard/');
+    $stateProvider.state('dashboard', {
+		url: '/dashboard/',
+		views: {
+		  'mainContent': {            
+		    templateUrl: 'views/dashboard.html',
+		    controller: 'dashboard'
+		  },
+		}
+	});
+});
