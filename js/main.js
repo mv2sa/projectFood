30b2f8130530db1cc2f44b311cedfd00ecbedfc3
Marcos Santanna <marcosv.santanna@hotmail.com>
2014-08-28 00:02:38 -0400

    broken state
    Being worked on

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var sessionStorageKeys = {
+	skeleton : 'skeleton_data'
+};
 var app = angular.module('findMyFood', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -17,14 +20,66 @@ app.config(function($stateProvider, $urlRouterProvider) {
 	});
 });
 
+app.controller('dashboard', function($scope, skeletonFactory) {
+	$scope.dashboardItems = {
+		loading : true
+	};
 
+	var init = function() {
+		var storedData;
 
-app.factory('skeleton', function($http) {
+		storedData = null;//skeletonFactory.getStoredSkeleton();
+		if(storedData === null) {
+			skeletonFactory.getSkeleton().then(function(d){
+				$scope.dashboardItems = d;
+			});
+		} else {
+			$scope.dashboardItems = storedData;
+		}
+		console.log($scope.dashboardItems);
+	};
+
+	init();
+});
+
+app.controller('menu', function($scope, skeletonFactory) {
+	$scope.menuItems = {
+		loading : true
+	};
+
+	var init = function() {
+		var storedData;
+
+		storedData = null;//skeletonFactory.getStoredSkeleton();
+		if(storedData === null) {
+			skeletonFactory.getSkeleton().then(function(d){
+				$scope.menuItems = d;
+			});
+		} else {
+			$scope.menuItems = storedData;
+		}
+		console.log($scope.menuItems);
+	};
+
+	init();
+});
+
+app.factory('skeletonFactory', function($http, $window) {
 
 	var factory = {};
 
-	factory.callConfigurations = function() {
+	/*factory.getStoredSkeleton = function() {
+		if ($window.Modernizr.sessionstorage) {
+			return $window.JSON.parse($window.sessionStorage.getItem($window.sessionStorageKeys.skeleton));
+		}
+	};*/
+
+	factory.getSkeleton = function() {
+
 		var promise = $http.get('json/skeleton.json').then(function (results) {
+			if ($window.Modernizr.sessionstorage) {
+				$window.sessionStorage.setItem($window.sessionStorageKeys.skeleton, $window.JSON.stringify(results.data));
+			}
 			return results.data;
 		});
 		return promise;
