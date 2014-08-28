a5efc230d21fb722d401790cb274802706a794de
mv2sa <marcosv.santanna@hotmail.com>
2014-08-28 01:01:06 -0400

    unbroken!!!
    fixed bizarre issues

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var sessionStorageKeys = {
 	skeleton : 'skeleton_data'
 };
-var app = angular.module('findMyFood', ['ui.router']);
+var app = angular.module('findMyFood', ['ui.router', 'filters']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/dashboard/');
@@ -28,37 +28,38 @@ app.controller('dashboard', function($scope, skeletonFactory) {
 	var init = function() {
 		var storedData;
 
-		storedData = null;//skeletonFactory.getStoredSkeleton();
+		storedData = skeletonFactory.getStoredSkeleton();
 		if(storedData === null) {
 			skeletonFactory.getSkeleton().then(function(d){
 				$scope.dashboardItems = d;
+				$scope.dashboardItems = false;
 			});
 		} else {
 			$scope.dashboardItems = storedData;
 		}
-		console.log($scope.dashboardItems);
+		
 	};
 
 	init();
 });
 
 app.controller('menu', function($scope, skeletonFactory) {
-	$scope.menuItems = {
+	$scope.navigation = {
 		loading : true
 	};
 
 	var init = function() {
 		var storedData;
 
-		storedData = null;//skeletonFactory.getStoredSkeleton();
+		storedData = skeletonFactory.getStoredSkeleton();
 		if(storedData === null) {
 			skeletonFactory.getSkeleton().then(function(d){
-				$scope.menuItems = d;
+				$scope.navigation = d;
+				$scope.navigation = false;
 			});
 		} else {
-			$scope.menuItems = storedData;
+			$scope.navigation = storedData;
 		}
-		console.log($scope.menuItems);
 	};
 
 	init();
@@ -68,14 +69,13 @@ app.factory('skeletonFactory', function($http, $window) {
 
 	var factory = {};
 
-	/*factory.getStoredSkeleton = function() {
+	factory.getStoredSkeleton = function() {
 		if ($window.Modernizr.sessionstorage) {
 			return $window.JSON.parse($window.sessionStorage.getItem($window.sessionStorageKeys.skeleton));
 		}
-	};*/
+	};
 
 	factory.getSkeleton = function() {
-
 		var promise = $http.get('json/skeleton.json').then(function (results) {
 			if ($window.Modernizr.sessionstorage) {
 				$window.sessionStorage.setItem($window.sessionStorageKeys.skeleton, $window.JSON.stringify(results.data));
@@ -87,3 +87,62 @@ app.factory('skeletonFactory', function($http, $window) {
 
 	return factory;
 });
+
+angular.module('filters', [])
+	.filter('underscoreToSpace', function () {
+		return function (input) {
+			if (input) {
+				return input.toLowerCase().replace(/_/g, ' ');
+			}
+		};
+	}
+).filter('toAZ', function () {
+		return function (input) {
+			if (input) {
+				if (input <= 26 && input >= 1) {
+					return String.fromCharCode(input + 64);
+				} else {
+					return input;
+				}
+			}
+		};
+	}
+).filter('ratingToStar', function () {
+		return function (input) {
+			if (typeof input === "number") {
+				if (input <= 5 && input >= 1) {
+					var fullStar = Math.round(input),
+						string = "";
+					for (var i = 0; i < fullStar; i++)  {
+						string = string + "&#xf005; "
+					} 
+					return string;
+				} else {
+					return input;
+				}
+			}
+		};
+	}
+).filter('priceRating', function () {
+		return function (input) {
+			if (typeof input === "number") {
+				if (input <= 4 && input >= 0) {
+					var string = "";
+					for (var i = 0; i <= input; i++)  {
+						string = string + "&#xf155; "
+					} 
+					return string;
+				} else {
+					return input;
+				}
+			}
+		};
+	}
+).filter('toHTML', ['$sce', function ($sce) {
+		return function (input) {
+			if (input) {
+				return $sce.trustAsHtml(input);
+			}
+		};
+	}]
+);
